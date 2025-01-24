32a3372fd76c4c77a1a6939506492baeece271c5
azvs.top@gmail.com <azvs.top@qq.com>
2025-01-24 21:28:31 +0800

    完成日志模块的大概轮廓. 还有一些想法没有实现, 记录在azvs_base.h 中.

---
+++ include/azvs_base.h
@@ -0,0 +1,105 @@
+#ifndef AZVS_BASE_H
+#define AZVS_BASE_H
+
+#include <stdio.h>
+
+//////////////////////////////////////////////////
+//     日志系统
+//////////////////////////////////////////////////
+
+//  #idea ...
+//  - 设置日志文件路径没有任何验证（strdup）
+//      + 路径设置有问题，对日志是毁灭性的
+//      + 若路径不存在，则创建文件夹（属于系统调用）
+//  - 考虑线程安全问题
+//      + Linux 和 Windows 的线程函数不同
+//      + C11 开始才有通用的线程函数标准库
+//      + 学习完后，考虑跨平台的线程解决方案
+//  - 将日志分类，存储在不同文件中（无实现思路）
+
+/**
+ * @brief 日志级别枚举
+ */
+typedef enum logger_level
+{
+    // 0000 0001 详细信息
+    A_TRACE = 1 << 0,
+    // 0000 0010 调试信息
+    A_DEBUG = 1 << 1,
+    // 0000 0100 一般信息
+    A_INFO = 1 << 2,
+    // 0000 1000 警告信息
+    A_WARN = 1 << 3,
+    // 0001 0000 错误信息
+    A_ERROR = 1 << 4,
+    // 0010 0000 严重错误信息
+    A_FATAL = 1 << 5,
+} A_LoggerLevel;
+
+/**
+ * @brief 日志输出方式
+ */
+typedef enum logger_output_type
+{
+    // 不输出日志文件
+    A_NONE = 0,
+    // 0000 0001 输出到控制台
+    A_CONSOLE = 1 << 0,
+    // 0000 0010 输出到文件
+    A_FILE = 1 << 1,
+} A_LoggerOutputType;
+
+typedef struct azvs_logger
+{
+    // 当前日志输出级别
+    A_LoggerLevel currentLoggerLevel;
+    // 当前日志输出方式
+    A_LoggerOutputType currentLoggerOutputType;
+    // 日志文件指针
+    FILE *outputFile;
+    // 日志文件路径
+    char *filePath;
+
+    // 修改当前日志输出级别
+    void (*setLoggerLevel)(const int level);
+    // 修改当前日志输出方式
+    void (*setLoggerOutputType)(const int type);
+    // 设置日志文件路径，请确定路径存在
+    void (*setFilePath)(const char *path);
+    // 输出日志信息，并设置当前日志行的级别
+    void (*loggerMessage)(const A_LoggerLevel level, const char *function, const int line, const char *format, ...);
+
+    // 初始化日志结构体
+    // （需要给函数指针赋值，只能放在结构体外部）
+    // void (*init)();
+    // 销毁日志结构体
+    void (*destroy)();
+
+} A_Logger;
+
+// 全局单例的日志对象
+extern A_Logger *a_logger_singleton;
+
+// 初始化单例日志对象
+A_Logger *a_initLogger();
+
+// 简化日志输出
+
+#ifdef DISABLE_LOGGER
+    #define A_LOG_MSG(level, format, ...) do {} while(0)
+#else
+    #define A_LOG_MSG(level, format, ...) \
+        do { \
+            if (a_logger_singleton != NULL) { \
+                a_logger_singleton->loggerMessage(level, __FUNCTION__, __LINE__, format, ##__VA_ARGS__); \
+            } \
+        } while (0)
+#endif // DISABLE_LOGGER
+#define A_LOG_MSG_TRACE(format, ...) A_LOG_MSG(A_TRACE, format, ##__VA_ARGS__)
+#define A_LOG_MSG_DEBUG(format, ...) A_LOG_MSG(A_DEBUG, format, ##__VA_ARGS__)
+#define A_LOG_MSG_INFO(format, ...) A_LOG_MSG(A_INFO, format, ##__VA_ARGS__)
+#define A_LOG_MSG_WARN(format, ...) A_LOG_MSG(A_WARN, format, ##__VA_ARGS__)
+#define A_LOG_MSG_ERROR(format, ...) A_LOG_MSG(A_ERROR, format, ##__VA_ARGS__)
+#define A_LOG_MSG_FATAL(format, ...) A_LOG_MSG(A_FATAL, format, ##__VA_ARGS__)
+
+#endif // !AZVS_BASE_H
---
+++ src/__base_logger.c
@@ -0,0 +1,216 @@
+#include "../include/azvs_base.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void __setLoggerLevel(const int level);
+void __setLoggerOutputType(const int type);
+void __setFilePath(const char *path);
+void __loggerMessage(const A_LoggerLevel level, const char *function, const int line, const char *format, ...);
+void __destroy();
+
+char *getLoggerLevelString(const A_LoggerLevel level);
+
+// 全局单例的日志对象
+A_Logger *a_logger_singleton;
+
+// 初始化日志对象
+A_Logger *a_initLogger()
+{
+    if (NULL == a_logger_singleton)
+    {
+        a_logger_singleton = (A_Logger *)malloc(sizeof(A_Logger));
+        if (!a_logger_singleton)
+        {
+            fprintf(stderr, "init -> 初始化日志对象时内存分配失败！\n");
+            return NULL;
+        }
+
+        a_logger_singleton->currentLoggerLevel = A_INFO | A_WARN | A_ERROR;
+        a_logger_singleton->currentLoggerOutputType = A_CONSOLE;
+        a_logger_singleton->filePath = strdup("./logs/default.log");
+        a_logger_singleton->outputFile = NULL;
+
+        a_logger_singleton->setLoggerLevel = __setLoggerLevel;
+        a_logger_singleton->setLoggerOutputType = __setLoggerOutputType;
+        a_logger_singleton->setFilePath = __setFilePath;
+        a_logger_singleton->loggerMessage = __loggerMessage;
+        a_logger_singleton->destroy = __destroy;
+    }
+    return a_logger_singleton;
+}
+
+void __setLoggerLevel(const int level)
+{
+    a_logger_singleton->currentLoggerLevel = level;
+}
+
+void __setLoggerOutputType(const int type)
+{
+    /*
+    如果 原(Y) AND 新(N)
+        关闭 FILE 文件
+    如果 原(N) AND 新(Y)
+        如果 PATH 存在
+            打开 FILE 文件
+        如果 PATH 不存在
+            exit(1)
+    */
+
+    if ((a_logger_singleton->currentLoggerOutputType & A_FILE) != 0 && (type & A_FILE) == 0)
+    {
+        if (fclose(a_logger_singleton->outputFile) != 0)
+        {
+            A_LOG_MSG_ERROR("日志文件 %s 关闭失败!\n", a_logger_singleton->filePath);
+        }
+        else
+        {
+            a_logger_singleton->outputFile = NULL;
+        }
+    }
+    if ((a_logger_singleton->currentLoggerOutputType & A_FILE) == 0 && (type & A_FILE) != 0)
+    {
+        if (a_logger_singleton->filePath != NULL)
+        {
+            a_logger_singleton->outputFile = fopen(a_logger_singleton->filePath, "a+");
+            if (a_logger_singleton->outputFile == NULL)
+            {
+                A_LOG_MSG_FATAL("日志文件 %s 打开失败！请确保路径存在！\n", a_logger_singleton->filePath);
+                exit(1);
+            }
+        }
+        else
+        {
+            A_LOG_MSG_FATAL("未正确设置日志文件路径！\n");
+            exit(1);
+        }
+    }
+    a_logger_singleton->currentLoggerOutputType = type;
+}
+
+void __setFilePath(const char *path)
+{
+    /*
+    如果 未开启日志文件模式
+        直接修改 path
+    如果 开启日志文件模式
+        关闭之前的日志文件
+        修改 path
+        打开新的日志文件
+    */
+
+    if ((a_logger_singleton->currentLoggerOutputType & A_FILE) == 0)
+    {
+        free(a_logger_singleton->filePath);
+        a_logger_singleton->filePath = strdup(path);
+    }
+    if ((a_logger_singleton->currentLoggerOutputType & A_FILE) != 0)
+    {
+        if (fclose(a_logger_singleton->outputFile) != 0)
+        {
+            A_LOG_MSG_ERROR("日志文件 %s 关闭失败！\n", a_logger_singleton->filePath);
+        }
+        else
+        {
+            a_logger_singleton->outputFile = NULL;
+        }
+        free(a_logger_singleton->filePath);
+        a_logger_singleton->filePath = strdup(path);
+        a_logger_singleton->outputFile = fopen(a_logger_singleton->filePath, "a+");
+        if (a_logger_singleton->outputFile == NULL)
+        {
+            A_LOG_MSG_FATAL("日志文件 %s 打开失败！请确保路径存在！\n", a_logger_singleton->filePath);
+            exit(1);
+        }
+    }
+}
+
+void __loggerMessage(const A_LoggerLevel level, const char *function, const int line, const char *format, ...)
+{
+    if (0 == (level & a_logger_singleton->currentLoggerLevel))
+    {
+        return;
+    }
+
+    char buffer[1024];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    va_end(args);
+
+    time_t nowtime;
+    struct tm *timeinfo;
+    time(&nowtime);
+    timeinfo = localtime(&nowtime);
+
+    // 控制台输出逻辑
+    if (A_CONSOLE & a_logger_singleton->currentLoggerOutputType)
+    {
+        printf("[%04d-%02d-%02d %02d:%02d:%02d][%s][%s:%d] -> %s",
+               timeinfo->tm_year + 1900,
+               timeinfo->tm_mon + 1,
+               timeinfo->tm_mday,
+               timeinfo->tm_hour,
+               timeinfo->tm_min,
+               timeinfo->tm_sec,
+               getLoggerLevelString(level),
+               function, line, buffer);
+    }
+    // 文件输出逻辑
+    if (A_FILE & a_logger_singleton->currentLoggerOutputType)
+    {
+        fprintf(a_logger_singleton->outputFile,
+                "[%04d-%02d-%02d %02d:%02d:%02d][%s][%s:%d] -> %s",
+                timeinfo->tm_year + 1900,
+                timeinfo->tm_mon + 1,
+                timeinfo->tm_mday,
+                timeinfo->tm_hour,
+                timeinfo->tm_min,
+                timeinfo->tm_sec,
+                getLoggerLevelString(level),
+                function, line, buffer);
+        fflush(a_logger_singleton->outputFile); // 确保内容写入文件
+    }
+}
+
+void __destroy()
+{
+    if (a_logger_singleton->outputFile)
+    {
+        if (fclose(a_logger_singleton->outputFile) != 0)
+        {
+            A_LOG_MSG_ERROR("日志文件 %s 关闭失败！\n", a_logger_singleton->filePath);
+        }
+        else
+        {
+            a_logger_singleton->outputFile = NULL;
+        }
+        free(a_logger_singleton->filePath);
+        a_logger_singleton->filePath = NULL;
+    }
+    free(a_logger_singleton);
+    a_logger_singleton = NULL;
+}
+
+char *getLoggerLevelString(const A_LoggerLevel level)
+{
+    switch (level)
+    {
+    case A_FATAL:
+        return "FATAL";
+    case A_ERROR:
+        return "ERROR";
+    case A_WARN:
+        return "WARN";
+    case A_INFO:
+        return "INFO";
+    case A_DEBUG:
+        return "DEBUG";
+    case A_TRACE:
+        return "TRACE";
+    default:
+        return "UNKNOWN";
+    }
+}
